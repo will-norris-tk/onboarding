11ba0c905caca5b54c5dab1f316630c549ed2491
William <william.norris@travelperk.com>
2024-02-01 14:44:42 +0000

    Nuke old security stuff

--- pre-commit-hooks/security/SAST/semgrep.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-docker ps > /dev/null 2>&1
-pip install -q semgrep
-[[ $? -ne 0 ]] && echo "Docker not available. Start docker daemon" && exit 1
-echo -e "\nRunning SAST semgrep\n"
-semgrep scan --config auto -q
