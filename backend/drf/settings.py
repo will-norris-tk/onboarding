9c90fa018805b158bbad93824a9752eb73f39114
William <william.norris@travelperk.com>
2024-02-05 09:56:10 +0000

    Cors config
    Allow access to backend from frontend app

--- backend/drf/settings.py
+++ backend/drf/settings.py
@@ -40,12 +40,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api.apps.ApiConfig',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -68,6 +70,11 @@ TEMPLATES = [
             ],
         },
     },
+]
+
+# Allow access from frontend
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
 ]
 
 WSGI_APPLICATION = 'drf.wsgi.application'
