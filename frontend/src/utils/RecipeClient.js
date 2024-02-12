85688ab769617192cb69f9ce947b12d6677d039c
William <william.norris@travelperk.com>
2024-02-12 18:21:23 +0000

    Client for handling http requests
    Could have removed some duplication in terms of request logic but this is ok for the time being. May add error handling later

---
+++ frontend/src/utils/RecipeClient.js
@@ -0,0 +1,54 @@
+import axios from 'axios';
+
+class RecipeClient {
+
+    baseUrl = 'http://localhost:8000/api/recipes/';
+
+    constructor(notifier) {
+        this.notifier = notifier;
+    }
+
+    _arrayOfObjectsToCommaSeparatedString(ingredients) {
+        return ingredients.map(ingredient => ingredient.name).join(',');
+    }
+    
+    _commaSeparatedStringToArrayOfObjects(ingredients) {
+        return ingredients.split(',').map(ingredient => {
+            return {name: ingredient};
+        });
+    }
+    
+
+    async getRecipes(setRecipes) {
+        const res = await axios.get(this.baseUrl);
+        res.data.map((recipe) => (recipe.ingredients = this._arrayOfObjectsToCommaSeparatedString(recipe.ingredients)) )
+        setRecipes(res.data);
+    }
+    
+    async createRecipe(recipe) {
+        await axios.post(this.baseUrl, {
+            name: recipe.name,
+            description: recipe.description,
+            ingredients: this._commaSeparatedStringToArrayOfObjects(recipe.ingredients)
+        });
+        this.notifier.notifyStateChange();
+    };
+    
+    async deleteRecipe(id) {
+        if (window.confirm('Are you sure you wish to delete this item?')) {
+            await axios.delete(`${this.baseUrl}${id}/`);
+            this.notifier.notifyStateChange();
+        }
+    };
+    
+    async updateRecipe(recipe) {
+        await axios.patch(`${this.baseUrl}${recipe.id}/`, {
+            name: recipe.name,
+            description: recipe.description,
+            ingredients: this._commaSeparatedStringToArrayOfObjects(recipe.ingredients)
+        });
+        this.notifier.notifyStateChange();
+    }
+}
+
+export default RecipeClient;
