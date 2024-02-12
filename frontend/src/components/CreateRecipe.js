a95ccaa5825a5b8633a473a8c2608c471d29442c
William <william.norris@travelperk.com>
2024-02-12 18:21:54 +0000

    Recipe creation component
    Should probably flash a message or show a toast on success but this is ok for now

---
+++ frontend/src/components/CreateRecipe.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import useInputState from '../hooks/useInputState';
+import { StyledForm } from './StyledForm';
+import { ControlledRecipeFields } from './ControlledRecipeFields';
+import { StyledRecipeDiv } from './StyledRecipeDiv';
+
+function CreateRecipe(props) {
+    const [recipe, updateRecipe, resetRecipe] = useInputState({
+        name: '',
+        description: '',
+        ingredients: ''
+    });
+
+    function handleSubmit(evt) {
+        evt.preventDefault();   
+        props.recipeClient.createRecipe(recipe);
+        resetRecipe();
+    }
+
+    return (
+        <StyledRecipeDiv>
+            <StyledForm onSubmit={handleSubmit}>
+                <ControlledRecipeFields recipeState={recipe} updateRecipeState={updateRecipe}/>
+                <button>Save</button>
+            </StyledForm>
+        </StyledRecipeDiv>
+    )
+}
+
+export default CreateRecipe;
