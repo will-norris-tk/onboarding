95701921ac1aa2a409f438974b29bcff1d7f2bed
William <william.norris@travelperk.com>
2024-02-12 18:22:38 +0000

    Build out application
    Add all of the new app logic and remove the logo file which is now useless

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import { NavLink, Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import './App.css';
+import CreateRecipe from './components/CreateRecipe';
+import { StyledRecipeFormContainer } from './components/StyledRecipeFormContainer';
+import Recipe from './components/EditRecipe.js';
+import RecipeClient from './utils/RecipeClient';
+import StateChangeNotifier from './utils/StateChangeNotifier';
+import { StyledNavBar } from './components/StyledNavBar.js';
 
 function App() {
+
+  const [recipes, setRecipes] = useState([]);
+  const [shouldReload, setShouldReload] = useState(false);
+  const notifier = new StateChangeNotifier(shouldReload, setShouldReload);
+  
+  
+  const recipeClient = new RecipeClient(notifier);
+  useEffect(() => 
+  {
+      recipeClient.getRecipes(setRecipes)  
+    // eslint-disable-next-line
+    }, [shouldReload]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+    <Router>
+      <StyledNavBar>
+          <NavLink exact to='/'>Recipe list</NavLink>
+          <NavLink exact to='/create'>Add new recipe</NavLink>
+      </StyledNavBar>
+        <Switch>
+          <Route exact path='/' >
+            <StyledRecipeFormContainer>
+                {recipes.map(
+                  recipe => (
+                      <Recipe key={recipe.id} recipe={recipe} recipeClient={recipeClient}/>
+                  )
+              )}
+            </StyledRecipeFormContainer>
+          </Route>
+          <Route exact path='/create'>
+            <StyledRecipeFormContainer>
+              <CreateRecipe recipeClient={recipeClient} />
+            </StyledRecipeFormContainer>
+          </Route>
+        </Switch>
+    </Router>
     </div>
   );
 }
