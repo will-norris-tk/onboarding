50367b9a645b807e64012dbd1ae695f10060097d
William <william.norris@travelperk.com>
2024-02-12 18:19:00 +0000

    Remove strictmode
    According to what I found online react router v5 and node 18 cause trouble when running in strictmode so removing it here since the exercise specifies using this version of react router

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,10 +5,10 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// Be wary - router v5 doesnt work with react 18 strictmode - https://github.com/remix-run/react-router/issues/7870
+// Adding strictmode here breaks navlinks in this project (at the very least)
 root.render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
