be15932617dd6b28797fe66890af31a5092ee9aa
William <william.norris@travelperk.com>
2024-02-13 11:09:11 +0000

    Error handling
    Catch any request errors and log them

--- frontend/src/utils/RecipeClient.js
+++ frontend/src/utils/RecipeClient.js
@@ -21,34 +21,50 @@ class RecipeClient {
     
 
     async getRecipes(setRecipes) {
-        const res = await axios.get(this.baseUrl);
-        res.data.map((recipe) => (recipe.ingredients = this._arrayOfObjectsToCommaSeparatedString(recipe.ingredients)) )
-        setRecipes(res.data);
+        try {
+            const res = await axios.get(this.baseUrl);
+            res.data.map((recipe) => (recipe.ingredients = this._arrayOfObjectsToCommaSeparatedString(recipe.ingredients)) )
+            setRecipes(res.data);
+        } catch (error) {
+            console.error(error);
+        }
     }
     
     async createRecipe(recipe) {
-        await axios.post(this.baseUrl, {
-            name: recipe.name,
-            description: recipe.description,
-            ingredients: this._commaSeparatedStringToArrayOfObjects(recipe.ingredients)
-        });
-        this.notifier.notifyStateChange();
-    };
+        try {
+            await axios.post(this.baseUrl, {
+                name: recipe.name,
+                description: recipe.description,
+                ingredients: this._commaSeparatedStringToArrayOfObjects(recipe.ingredients)
+            });
+            this.notifier.notifyStateChange();
+        } catch (error) {
+            console.error(error);
+        }
+    }
     
     async deleteRecipe(id) {
         if (window.confirm('Are you sure you wish to delete this item?')) {
-            await axios.delete(`${this.baseUrl}${id}/`);
-            this.notifier.notifyStateChange();
+            try {
+                await axios.delete(`${this.baseUrl}${id}/`);
+                this.notifier.notifyStateChange();
+            } catch (error) {
+                console.error(error);
+            }
         }
-    };
+    }
     
     async updateRecipe(recipe) {
-        await axios.patch(`${this.baseUrl}${recipe.id}/`, {
-            name: recipe.name,
-            description: recipe.description,
-            ingredients: this._commaSeparatedStringToArrayOfObjects(recipe.ingredients)
-        });
-        this.notifier.notifyStateChange();
+        try { 
+            await axios.patch(`${this.baseUrl}${recipe.id}/`, {
+                name: recipe.name,
+                description: recipe.description,
+                ingredients: this._commaSeparatedStringToArrayOfObjects(recipe.ingredients)
+            });
+            this.notifier.notifyStateChange();
+        } catch (error) {
+            console.error(error);
+        }
     }
 }
 
